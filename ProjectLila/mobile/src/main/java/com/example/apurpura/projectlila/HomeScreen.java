f70f18a23b678512b714d42d91d195e40910f971
laniangela <laniangela@gmail.com>
2017-06-01 22:00:11 -0400

    First Try Home Screen and Do Something Edit/Create
    Initial code push for UX/UI - 2 Activity Screens

--- ProjectLila/mobile/src/main/java/com/example/apurpura/projectlila/HomeScreen.java
+++ ProjectLila/mobile/src/main/java/com/example/apurpura/projectlila/HomeScreen.java
@@ -1,52 +1,66 @@
 package com.example.apurpura.projectlila;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lania on 05/30/2017.
+ */
 
 public class HomeScreen extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_screen);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setContentView(R.layout.toolbar_bottom);
+        Toolbar bottomToolbar = (Toolbar) findViewById(R.id.toolbar_bottom);
+        setSupportActionBar(bottomToolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        bottomToolbar.setTitle("");
+        bottomToolbar.setSubtitle("");
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home_screen, menu);
+        getMenuInflater().inflate(R.menu.toolbar_bottom, menu);
         return true;
     }
 
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch (item.getItemId()) {
+            case R.id.action_sequence_something:
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+                return true;
+
+            case R.id.action_say_something:
+
+                return true;
+
+            case R.id.action_be_somewhere:
+
+                return true;
+
+            case R.id.action_do_something:
+
+                return true;
+
+            case R.id.action_get_metrics:
+
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
 }
