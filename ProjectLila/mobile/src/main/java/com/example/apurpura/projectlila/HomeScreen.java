cd1d32f77d8a7b723097ea9003c798a1b9394514
laniangela <laniangela@gmail.com>
2017-06-03 17:39:34 -0400

    HomeScreen and Cleanup
    - Added an ActionBar for the topbar
    - chaged bottomToolbar to an extention of the Main Home Screen Menu as a Action Menu View rather than a toolbar unto itself
    - deleted toolbar_bottom layout as not using as an insert of the Home Screen anymore
    - removed all layout references fot he tollbar_bottom layout

--- ProjectLila/mobile/src/main/java/com/example/apurpura/projectlila/HomeScreen.java
+++ ProjectLila/mobile/src/main/java/com/example/apurpura/projectlila/HomeScreen.java
@@ -1,10 +1,13 @@
 package com.example.apurpura.projectlila;
 
 import android.os.Bundle;
+import android.support.v7.widget.ActionMenuView;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.app.AppCompatActivity;
+
 
 /**
  * Created by lania on 05/30/2017.
@@ -16,55 +19,75 @@ public class HomeScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.toolbar_bottom);
-        Toolbar bottomToolbar = (Toolbar) findViewById(R.id.toolbar_bottom);
-        setSupportActionBar(bottomToolbar);
+        setContentView(R.layout.home_screen);
+        Toolbar toolbarTop = (Toolbar) findViewById(R.id.menu_home_screen);
+        setSupportActionBar(toolbarTop);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
-        bottomToolbar.setTitle("");
-        bottomToolbar.setSubtitle("");
+        toolbarTop.setTitle("");
+        toolbarTop.setSubtitle("");
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.toolbar_bottom, menu);
+        // Inflate the topToolbar menu
+        getMenuInflater().inflate(R.menu.menu_home_screen, menu);
+
+        // Inflate and initialize the bottom menu
+        ActionMenuView bottomBar = (ActionMenuView)findViewById(R.id.bottom_toolbar);
+        Menu toolbarBottom = bottomBar.getMenu();
+        getMenuInflater().inflate(R.menu.toolbar_bottom, toolbarBottom);
+        for (int i = 0; i < toolbarBottom.size(); i++) {
+            toolbarBottom.getItem(i).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                @Override
+               public boolean onMenuItemClick(MenuItem item) {
+ /*                   switch (item.getItemId()) {
+                        case R.id.action_sequence_something:
+
+                            return true;
+
+                        case R.id.action_say_something:
+
+                            return true;
+
+                        case R.id.action_be_somewhere:
+
+                            return true;
+
+                        case R.id.action_do_something:
+
+                            return true;
+
+                        case R.id.action_get_metrics:
+
+                            return true;
+
+                       default:
+                            // If we got here, the user's action was not recognized.
+                            // Invoke the superclass to handle it.
+                            return super.onOptionsItemSelected(item);*/
+                    return onOptionsItemSelected(item);
+                }
+            });
+        }
+
         return true;
     }
-    //SOME COMMENT Add more comment
-
-    //some comment
 
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_sequence_something:
+       @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            int id = item.getItemId();
 
+            //noinspection SimplifiableIfStatement
+            if (id == R.id.action_settings) {
                 return true;
+            }
 
-            case R.id.action_say_something:
-
-                return true;
-
-            case R.id.action_be_somewhere:
-
-                return true;
-
-            case R.id.action_do_something:
-
-                return true;
-
-            case R.id.action_get_metrics:
-
-                return true;
-
-            default:
-                // If we got here, the user's action was not recognized.
-                // Invoke the superclass to handle it.
-                return super.onOptionsItemSelected(item);
-
+            return super.onOptionsItemSelected(item);
         }
-    }
 
 }
